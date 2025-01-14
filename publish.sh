9501bf750a7c974a658ff63986a05e04125e0109
Didem Benlioglu <didem.benlioglu@gmail.com>
2025-01-14 21:28:27 +0100

    -

--- publish.sh
+++ publish.sh
@@ -4,7 +4,8 @@ FILE="cities.txt"
 
 while IFS= read -r CITY
 do
-  echo "City: " $CITY
+  echo "City: " $CITY 
   WEATHER=$(./get_weather.sh "$CITY")
+  echo "Weather:" $WEATHER 
   curl -X POST http://192.168.178.155:5000/add -H "Content-Type: application/json" -d "{\"city\": \"$CITY\", \"weather\": \"$WEATHER\"}"
 done < "$FILE"
