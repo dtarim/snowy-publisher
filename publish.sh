5744f2983d6760035c5add35649b90b8435566bd
Didem Benlioglu <didem.benlioglu@gmail.com>
2025-01-14 21:18:04 +0100

    -

--- publish.sh
+++ publish.sh
@@ -6,5 +6,5 @@ while IFS= read -r CITY
 do
   echo "City: " $CITY
   WEATHER=$(./get_weather.sh "$CITY")
-  curl -X POST http://192.168.178.86:5000/add -H "Content-Type: application/json" -d "{\"city\": \"$CITY\", \"weather\": \"$WEATHER\"}"
+  curl -X POST http://192.168.178.155:5000/add -H "Content-Type: application/json" -d "{\"city\": \"$CITY\", \"weather\": \"$WEATHER\"}"
 done < "$FILE"
